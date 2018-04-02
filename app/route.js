eeb0b0351b07f492a62e824d621262bf57e7d3d3
kgonzalez452 <kgonzalez@techlaunch.io>
2018-04-02 02:23:44 -0400

    server working properly

--- app/route.js
+++ app/route.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('helloworld.ejs');
+        res.render('./helloworld.ejs');
+        // res.send('hello world');
     });
 };
